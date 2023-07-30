cfed52dbfd44f264b785905824726ddd1d2e7aba
AndMay <am@42.ru>
2023-07-30 21:37:17 +0700

    added procedure PrintArray

--- Program.cs
+++ Program.cs
@@ -3,13 +3,12 @@
 //         При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
 
 int ReadInt(string message)
-    {
-        Console.Write(message + " ");
-        return Convert.ToInt32(Console.ReadLine());
-    }
+{
+    Console.Write(message + " ");
+    return Convert.ToInt32(Console.ReadLine());
+}
 
 void ReadArray(string[] ArrayForRead)
-
 {
     Console.WriteLine("Строк для ввода " + ArrayForRead.Length);
     for (int i = 0; i < ArrayForRead.Length; i++)
@@ -19,6 +18,10 @@ void ReadArray(string[] ArrayForRead)
     }
 }
 
+void PrintArray(string[] ArrayForPrint)
+{
+    Console.WriteLine($"[{string.Join(", ", ArrayForPrint)}]");
+}
 
 
 //============================================================================
@@ -27,5 +30,7 @@ int size = ReadInt("Введите размер массива: ");
 string[] arrayOfString = new string[size];
 
 ReadArray(arrayOfString);
+Console.Write("Исходный массив: ");
+PrintArray(arrayOfString);
 
 
