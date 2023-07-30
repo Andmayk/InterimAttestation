32e3ec8ddad4b9d7342b5836ae464e41f0ad1d9c
AndMay <am@42.ru>
2023-07-30 21:30:26 +0700

    added procedure ReadArray

--- Program.cs
+++ Program.cs
@@ -1,2 +1,31 @@
-﻿co// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам. 
+//         Первоначальный массив можно ввести с клавиатуры,  либо задать на старте выполнения алгоритма. 
+//         При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+int ReadInt(string message)
+    {
+        Console.Write(message + " ");
+        return Convert.ToInt32(Console.ReadLine());
+    }
+
+void ReadArray(string[] ArrayForRead)
+
+{
+    Console.WriteLine("Строк для ввода " + ArrayForRead.Length);
+    for (int i = 0; i < ArrayForRead.Length; i++)
+    {
+        Console.Write((i + 1) + ": ");
+        ArrayForRead[i] = Console.ReadLine();
+    }
+}
+
+
+
+//============================================================================
+int size = ReadInt("Введите размер массива: ");
+
+string[] arrayOfString = new string[size];
+
+ReadArray(arrayOfString);
+
+
